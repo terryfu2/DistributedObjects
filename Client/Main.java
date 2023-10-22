1b0bb230d643f15f23803e565d8da9496b795b4c
Terry Fu <terryfu33@gmail.com>
2023-10-22 17:19:54 -0600

    updated object tracking

--- Client/Main.java
+++ Client/Main.java
@@ -1,10 +1,24 @@
 package Client;
 
+import java.util.ArrayList;
+
 public class Main {
 	
     public static void main(String[] argv) throws Exception {
     	
     	ObjectCreator objectCreator = new ObjectCreator();
+    	ArrayList<Object> objects = objectCreator.getSelectedObjects();
+    	printObjects(objects);
     }
-   
+    
+    public static void printObjects(ArrayList<Object> objects) {
+    	
+		System.out.println("selected Objects ...");
+
+    	for(Object obj:objects) {
+    		
+    		
+    		System.out.println(obj.getClass().getSimpleName());
+    	}
+    }
 }
--- Client/ObjectCreator.java
+++ Client/ObjectCreator.java
@@ -13,23 +13,25 @@ public class ObjectCreator {
 	
 	private ArrayList<Class> classes = new ArrayList<>();;
 	private ArrayList<Class> selectedClasses = new ArrayList<>();;
-
+	private ArrayList<Object> selectedObjects = new ArrayList<>();;
 	
 	
 	public ObjectCreator() throws Exception {
 		
 		System.out.println("Object Creator: ");
 		this.createClasses();
-		System.out.println("classes avaliable ...");
 		this.printClasses(classes);
 		this.userInput();
-		System.out.println("selected Objects ...");
-		this.printClasses(selectedClasses);
 	}
 	
 	public ArrayList<Class> getSelectedClasses(){
 		
 		return this.selectedClasses;
+	}
+	
+	public ArrayList<Object> getSelectedObjects(){
+		
+		return this.selectedObjects;
 	}
 	
 	public void userInput() throws Exception {
@@ -46,22 +48,28 @@ public class ObjectCreator {
 			if(curr == '0') {
 				ClassA classA = new ClassA();
 				selectedClasses.add(classA.getClass());
+				selectedObjects.add(classA);
 			}
 			else if(curr == '1') {
 				ClassB classB = new ClassB();
 				selectedClasses.add(classB.getClass());
+				selectedObjects.add(classB);
 			}
 			else if(curr == '2') {
 				ClassD classD = new ClassD();
 				selectedClasses.add(classD.getClass());
+				selectedObjects.add(classD);
 			}
 			else if(curr == '3') {
 				Client client = new Client("asdf",0);
 				selectedClasses.add(client.getClass());
+				selectedObjects.add(client);
 			}
 			else if(curr == '4') {
 				Server server = new Server(0);
 				selectedClasses.add(server.getClass());
+				selectedObjects.add(server);
+
 			}
 	    }
 		
@@ -80,11 +88,11 @@ public class ObjectCreator {
 		classes.add(client.getClass());
 		classes.add(server.getClass());
 
-
 	}
 	
 	public void printClasses(ArrayList<Class> list) {
 		
+		System.out.println("classes avaliable ...");
 
 		int count = 0;
 		for(Class curr:list) {
